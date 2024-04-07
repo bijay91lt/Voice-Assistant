87f0f6e8a5cfacf7a93e84293ad532b302ce4f60
kakeru91lt <bijay3990@gmail.com>
2024-04-07 06:40:38 +0545

    Add new feature

--- Murphy_91lt/main.py
+++ Murphy_91lt/main.py
@@ -3,64 +3,80 @@
 #git commit -m "Add new feature"
 #git push origin main
 
-
+from flask import Flask, render_template, request
 from packages.Va_Functions import VA_func
 
-def main():
-    va1 = VA_func()
-    va1.wish_me()
+app = Flask(__name__)
 
-    while True:
-        command = va1.take_command().lower()
+app.static_url_path= '/static'
+app.static_folder = 'static'
 
-        if "wikipedia" in command:
-            va1.search_wikipedia(command)
-        elif "google" in command:
-            va1.search_google(command)
-        elif "youtube" in command:
-            va1.search_youtube(command)
-        elif "music" in command:
-            va1.play_music()
-        elif "time" in command:
-            va1.time_now()
-        elif "date" in command:
-            va1.time_and_date()
-        elif "notepad" in command:
-            va1.open_notepad()
-        elif "exit" in command:
-            va1.exit_assistant()
-        elif "joke" in command:
-            va1.tell_joke()
-        elif "translation" in command:
-            va1.voice_translation()
-        elif "background" in command:
-            va1.change_bg()
-        elif "news" in command:
-            va1.news()
-        elif "lock" in command:
-            va1.lock_window()
-        elif "shutdown" in command:
-            va1.shut_down()
-        elif "empty bin" in command:
-            va1.empty_bin()
-        elif "sleep" in command:
-            va1.sleep()
-        elif "where is" in command:
-            va1.locate(command)
-        elif "picture" in command or "photo" in command:
-            va1.camera()
-        elif "take a note" in command:
-            va1.write_note()
-        elif "show note" in command:
-            va1.show_note()
-        elif "restart" in command:
-            va1.restart()
-        elif "logout" in command:
-            va1.logout()
-        elif "weather" in command:
-            va1.get_weather()
-        else:
-            va1.speak("Sorry, I didn't get that. Can you repeat?")
-            
-if __name__ == "__main__":
-    main()
+
+va1 = VA_func()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit_command', methods=['POST'])
+def submit_command():
+    
+    data = request.get_json()
+    command = data['command'].lower()
+    response = handle_command(command)
+    return response
+
+def handle_command(command):
+    if "wikipedia" in command:
+        return va1.search_wikipedia(command)
+    elif "hello" in command:
+        return va1.wish_me()
+    elif "google" in command:
+        return va1.search_google(command)
+    elif "youtube" in command:
+        return va1.search_youtube(command)
+    elif "music" in command:
+        return va1.play_music()
+    elif "time" in command:
+        return va1.time_now()
+    elif "date" in command:
+        return va1.time_and_date()
+    elif "notepad" in command:
+        return va1.open_notepad()
+    elif "exit" in command:
+        return va1.exit_assistant()
+    elif "joke" in command:
+        return va1.tell_joke()
+    elif "translation" in command:
+        return va1.voice_translation()
+    elif "background" in command:
+        return va1.change_bg()
+    elif "news" in command:
+        return va1.news()
+    elif "lock" in command:
+        return va1.lock_window()
+    elif "shutdown" in command:
+        return va1.shut_down()
+    elif "empty bin" in command:
+        return va1.empty_bin()
+    elif "sleep" in command:
+        return va1.sleep()
+    elif "where is" in command:
+        return va1.locate(command)
+    elif "picture" in command or "photo" in command:
+        return va1.camera()
+    elif "take a note" in command:
+        return va1.write_note()
+    elif "show note" in command:
+        return va1.show_note()
+    elif "restart" in command:
+        return va1.restart()
+    elif "logout" in command:
+        return va1.logout()
+    elif "weather" in command:
+        return va1.get_weather()
+    else:
+        return "Sorry, I didn't get that. Can you repeat?"
+
+if __name__ == '__main__':
+    app.run(debug=True)
